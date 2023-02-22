2943d6d593de7d3938207961c8b5855a6dece8c6
ivan <ivan.safonov.09@mail.ru>
2023-02-22 11:00:32 +0300

    logout front

--- front/app/components/GlobalHeader.tsx
+++ front/app/components/GlobalHeader.tsx
@@ -1,12 +1,33 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import React from 'react'
 import logout from '../../public/svg/logout.svg'
+import { AuthApi } from '../screens/Auth/AuthApi/AuthApi'
 import s from './GlobalHeader.module.scss'
 const GlobalHeader = () => {
+  const route = useRouter()
+
+  const logoutFun = async () => {
+    try {
+      await AuthApi.logout()
+      localStorage.clear()
+      route.replace('/auth')
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <div className={s.cont}>
       <p>Todo List</p>
-      <Image src={logout} width={30} height={30} alt='logout' className={s.img}/>
+      <Image
+        onClick={logoutFun}
+        src={logout}
+        width={30}
+        height={30}
+        alt='logout'
+        className={s.img}
+      />
     </div>
   )
 }
