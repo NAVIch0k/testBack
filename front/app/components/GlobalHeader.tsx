ad36aecea0ceb2f4b45a645560a65be674145267
ivan <ivan.safonov.09@mail.ru>
2023-02-24 22:34:53 +0300

    fix

--- front/app/components/GlobalHeader.tsx
+++ front/app/components/GlobalHeader.tsx
@@ -3,13 +3,16 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import logout from '../../public/svg/logout.svg'
 import { AuthApi } from '../screens/Auth/AuthApi/AuthApi'
+import { useUserStore } from '../screens/Auth/store/useUserStore'
 import s from './GlobalHeader.module.scss'
 const GlobalHeader = () => {
   const route = useRouter()
+  const clearUser = useUserStore((state) => state.setUser)
 
   const logoutFun = async () => {
     try {
       await AuthApi.logout()
+      clearUser({role:'',userName:''})
       localStorage.clear()
       route.replace('/auth')
     } catch (e) {
